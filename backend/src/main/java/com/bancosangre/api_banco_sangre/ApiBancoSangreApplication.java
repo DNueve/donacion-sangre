ca960ae8bf2ded0ff582dd9dd287a8ce27c1ea2f
Carlos-web322 <carlos_diaz82161@elpoli.edu.co>
2026-05-05 22:44:34 -0500

    feat: agregar backend Spring Boot al monorepo

---
+++ backend/src/main/java/com/bancosangre/api_banco_sangre/ApiBancoSangreApplication.java
@@ -0,0 +1,13 @@
+package com.bancosangre.api_banco_sangre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoSangreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancoSangreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bancosangre/api_banco_sangre/ApiBancoSangreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancosangre.api_banco_sangre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoSangreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
